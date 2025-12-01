724185764e10a0d3bc5b80481501c3fca5543c91
Zlata Sinyayeva <zlataz420@gmail.com>
2025-12-01 18:32:16 +0000

    Ask for user name in hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,18 @@
-name="appsec world"
+import typer
+
+def main(
+    name: str,
+    lastname: str = typer.Option("", help="Фамилия пользователя."),
+    formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
+):
+    """
+    Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
+    """
+    if formal:
+        print(f"Добрый день, {name} {lastname}!")
+    else:
+        print(f"Привет, {name}!")
+
+if name == "main":
+    typer.run(main)name="appsec world"
 print("Hello " + name)
