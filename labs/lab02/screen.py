26708084f1b1f995062e8372d5241b3a4cf90212
Zlata Sinyayeva <zlataz420@gmail.com>
2025-12-09 13:22:41 +0000

    Add screen.py

---
+++ labs/lab02/screen.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+bg_color = (255, 255, 255)
+pygame.draw.rect(screen, bg_color, [0, 0, screen_width, screen_height], 1)
+
+# Устанавливаем размеры окна
+screen_width = 800
+screen_height = 600
+window_size = (screen_width, screen_height)
+pygame.display.set_mode(window_size) # Создаем окно
+
+# Задаем цвет фона
+bg_color = (255, 255, 255)
+pygame.draw.rect(screen, bg_color, [0, 0, screen_width, screen_height], 1)
+
+# Выводим текст на экран
+font = pygame.font.SysFont(None, 75)
+text = font.render("Hello appsec world*", True, (0, 255, 0))
+text_rect = text.get_rect()
+text_rect.center = (400, 300)
+screen.blit(text, text_rect)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+pygame.display.flip() # Обновляем экран
