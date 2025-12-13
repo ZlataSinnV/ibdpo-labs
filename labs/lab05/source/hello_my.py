7e512a88f1fe9bbf20419c02d91f422eed17cf2a
Zlata Sinyayeva <zlataz420@gmail.com>
2025-12-13 17:58:19 +0000

    Add hello_my.py and changed Dockerfile

---
+++ labs/lab05/source/hello_my.py
@@ -0,0 +1,26 @@
+# Module import
+
+import typer
+
+
+# Function "main"
+
+def main(
+    name: str,
+    lastname: str = typer.Option("", help="Фамилия пользователя."),
+    formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
+):
+    """
+    Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
+    """
+    
+    if formal:
+        print(f"Добрый день, {name} {lastname}!")
+    else:
+        print(f"Привет, {name}!")
+
+
+# Start point of this program
+
+if __name__ == "__main__":
+    typer.run(main)
